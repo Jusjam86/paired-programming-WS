fcf5baaa4b464577bd679cc959b3363db2f589e0
ariel raymundo <ariraymundo98@gmail.com>
2026-05-15 11:28:05 -0700

    working on SalesContract class logic pls help :3

--- car-dealership-pair/src/main/java/com/pluralsight/models/SalesContract.java
+++ car-dealership-pair/src/main/java/com/pluralsight/models/SalesContract.java
@@ -3,6 +3,7 @@ package com.pluralsight.models;
 public abstract class SalesContract extends Contract {
 
     // variables
+    private Vehicle vehicle; //vehicle object
     private double totalPrice;
     private double monthlyPayment;
     private double salesTaxAmount = 0.05;
@@ -14,7 +15,7 @@ public abstract class SalesContract extends Contract {
     // constructor
     public SalesContract(String dateOfContract, String customerName, String customerEmail, boolean vehicleSold,
                          double totalPrice, double monthlyPayment, double salesTaxAmount, double recordingFee,
-                         double processingFee, boolean finance, boolean isMonthlyPaymentFinanced)
+                         double processingFee, Vehicle vehicle, boolean finance, boolean isMonthlyPaymentFinanced)
     {
         super(dateOfContract, customerName, customerEmail, vehicleSold);
         this.totalPrice = totalPrice;
@@ -22,6 +23,7 @@ public abstract class SalesContract extends Contract {
         this.salesTaxAmount = salesTaxAmount;
         this.recordingFee = recordingFee;
         this.processingFee = processingFee;
+        this.vehicle = vehicle; // vehicle object
         this.finance = finance;
         this.isMonthlyPaymentFinanced = isMonthlyPaymentFinanced;
     }
@@ -33,17 +35,35 @@ public abstract class SalesContract extends Contract {
     public double getRecordingFee() {return recordingFee;}
     public void setRecordingFee(double recordingFee) {this.recordingFee = recordingFee;}
 
-    public double getProcessingFee() {return processingFee;}
+    public double getProcessingFee()
+    {
+        if (vehicle.getPrice() < 10000){
+            return 295;
+        }
+        if (vehicle.getPrice() > 10000){
+            return 495;
+        }
+        return 0.00;
+    }
+
     public void setProcessingFee(double processingFee) {this.processingFee = processingFee;}
 
     public boolean isFinance() {return finance;}
+
     public void setFinance(boolean finance) {this.finance = finance;}
 
-    public boolean isMonthlyPaymentFinanced() {return isMonthlyPaymentFinanced;}
+    public boolean isMonthlyPaymentFinanced()
+    {
+        return isMonthlyPaymentFinanced;
+    }
+
     public void setMonthlyPaymentFinanced(boolean monthlyPaymentFinanced) {isMonthlyPaymentFinanced = monthlyPaymentFinanced;}
 
     @Override
-    public abstract double getTotalPrice();
+    public double getTotalPrice()
+    {
+        return totalPrice;
+    };
 
     @Override
     public abstract double getMonthlyPayment();
