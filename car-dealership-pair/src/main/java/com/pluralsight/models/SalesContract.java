e7a88add1813d766a8b476ab9e0bbfac19bf2b7e
ariel raymundo <ariraymundo98@gmail.com>
2026-05-15 12:59:18 -0700

    SalesContract class complete, probably

--- car-dealership-pair/src/main/java/com/pluralsight/models/SalesContract.java
+++ car-dealership-pair/src/main/java/com/pluralsight/models/SalesContract.java
@@ -1,38 +1,30 @@
 package com.pluralsight.models;
 
-public abstract class SalesContract extends Contract {
+public class SalesContract extends Contract {
 
     // variables
-    private Vehicle vehicle; //vehicle object
-    private double totalPrice;
-    private double monthlyPayment;
+    private Vehicle vehicle; //vehicle object;
     private double salesTaxAmount = 0.05;
     private double recordingFee = 100;
     private double processingFee;
     private boolean finance; // y/n
-    private boolean isMonthlyPaymentFinanced;
 
     // constructor
-    public SalesContract(String dateOfContract, String customerName, String customerEmail, boolean vehicleSold,
-                         double totalPrice, double monthlyPayment, double salesTaxAmount, double recordingFee,
-                         double processingFee, Vehicle vehicle, boolean finance, boolean isMonthlyPaymentFinanced)
+    public SalesContract(String dateOfContract, String customerName, String customerEmail,
+                         boolean vehicleSold, Vehicle vehicle, boolean finance)
     {
         super(dateOfContract, customerName, customerEmail, vehicleSold);
-        this.totalPrice = totalPrice;
-        this.monthlyPayment = monthlyPayment;
-        this.salesTaxAmount = salesTaxAmount;
-        this.recordingFee = recordingFee;
-        this.processingFee = processingFee;
-        this.vehicle = vehicle; // vehicle object
+        this.vehicle = vehicle;
         this.finance = finance;
-        this.isMonthlyPaymentFinanced = isMonthlyPaymentFinanced;
     }
 
     // getters & setters
     public double getSalesTaxAmount() {return salesTaxAmount;}
+
     public void setSalesTaxAmount(double salesTaxAmount) {this.salesTaxAmount = salesTaxAmount;}
 
     public double getRecordingFee() {return recordingFee;}
+
     public void setRecordingFee(double recordingFee) {this.recordingFee = recordingFee;}
 
     public double getProcessingFee()
@@ -52,19 +44,32 @@ public abstract class SalesContract extends Contract {
 
     public void setFinance(boolean finance) {this.finance = finance;}
 
-    public boolean isMonthlyPaymentFinanced()
-    {
-        return isMonthlyPaymentFinanced;
-    }
-
-    public void setMonthlyPaymentFinanced(boolean monthlyPaymentFinanced) {isMonthlyPaymentFinanced = monthlyPaymentFinanced;}
+    // override methods
 
     @Override
     public double getTotalPrice()
     {
-        return totalPrice;
-    };
+        return vehicle.getPrice() + (vehicle.getPrice() * salesTaxAmount) + recordingFee + getProcessingFee();
+    }
 
     @Override
-    public abstract double getMonthlyPayment();
+    public double getMonthlyPayment()
+    {
+        // for if user doesn't want to finance vehicle
+        if (!finance) {return 0;}
+
+        // calculations for the monthly payment using the vehicle's total price
+        double totalPrice = getTotalPrice();
+
+        if (totalPrice >= 10000)
+        {
+            // new double variable
+            return totalPrice / 48 * (1 + 0.0425);
+        }
+        else
+        {
+            return totalPrice / 24 * (1 + 0.0525);
+        }
+
+    }
 }
